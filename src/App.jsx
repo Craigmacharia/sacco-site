3ebf528a46f1141456d84e9d347f616f34dd1358
craigmacharia <cmacharia482@gmail.com>
2025-07-19 17:56:45 +0300

    ssj2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import Calculator from "./components/calculator";
+import Calculator from "./components/Calculator";
 
 // Pages
 import Home from "./pages/Home";
